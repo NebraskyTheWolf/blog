3305e484ece3b4e30366eddfe19bcab5be3685a7
NebraskyTheWolf <farfy.dev@gmail.com>
2024-05-16 22:54:34 +0200

    Update software license and apply to multiple files
    This commit changes the software license used in the project from "SOFTWARE PRODUCT PROPRIETARY LICENSE" to a new proprietary license and applies this update across various types of files including PHP, CSS, and SCSS files.
    
    The newly implemented license is in Czech and includes specific terms and conditions for usage, reproduction, distribution, and software modifications. A link is provided within the license for viewing the complete license text online.

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -1,27 +1,24 @@
 @php use Carbon\Carbon; @endphp
 
 <!--
-    ===========================================
-     SOFTWARE PRODUCT PROPRIETARY LICENSE
-    ===========================================
+                            LICENCE PRO PROPRIETÁRNÍ SOFTWARE
+              Verze 1, Organizace: Fluffici, z.s. IČO: 19786077, Rok: 2024
+                            PODMÍNKY PRO POUŽÍVÁNÍ
 
-    Fluffici, z.s., IČO: 19786077, Year: 2024
+    a. Použití: Software lze používat pouze podle přiložené dokumentace.
+    b. Omezení reprodukce: Kopírování softwaru bez povolení je zakázáno.
+    c. Omezení distribuce: Distribuce je povolena jen přes autorizované kanály.
+    d. Oprávněné kanály: Distribuci určuje výhradně držitel autorských práv.
+    e. Nepovolené šíření: Šíření mimo povolené podmínky je zakázáno.
+    f. Právní důsledky: Porušení podmínek může vést k právním krokům.
+    g. Omezení úprav: Úpravy softwaru jsou zakázány bez povolení.
+    h. Rozsah oprávněných úprav: Rozsah úprav určuje držitel autorských práv.
+    i. Distribuce upravených verzí: Distribuce upravených verzí je povolena jen s povolením.
+    j. Zachování autorských atribucí: Kopie musí obsahovat všechny autorské atribuce.
+    k. Zodpovědnost za úpravy: Držitel autorských práv nenese odpovědnost za úpravy.
 
-    DEVELOPER INFORMATION:
-    Developer Name: Vakea
-    Contact Information: vakea@fluffici.eu
-
-    TERMS AND CONDITIONS FOR USAGE
-
-    1. You may only use the Software Product in accordance with the accompanying documentation.
-    2. Reproduction and distribution of the Software Product are strictly prohibited, except through official channels defined and controlled by the copyright holder. Any illegal reproduction or distribution will be subject to legal action.
-    3. You may not modify or make derivative works of the Software Product.
-    4. You must retain all copyright, trademark, and proprietary notices on all copies of the Software Product made, if any.
-    5. You are not granted any rights to any trademarks or service marks of the Software Product.
-
-    THIS SOFTWARE PRODUCT IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-
-    ===========================================
+    Celý text licence je dostupný na adrese:
+    https://autumn.fluffici.eu/attachments/xUiAJbvhZaXW3QIiLMFFbVL7g7nPC2nfX7v393UjEn/fluffici_software_license_cz.pdf
 -->
 
 <!DOCTYPE html>
